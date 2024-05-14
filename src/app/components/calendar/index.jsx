ed79d6649d925cc44b46637338ffff6b07b0ebb7
Manita Paudel <paudel04manita@gmail.com>
2024-05-14 12:41:39 +0545

    Add onclick handlers to the prev and next buttons, show specific month and year based on the button clicks

--- src/app/components/calendar/index.jsx
+++ src/app/components/calendar/index.jsx
@@ -1,17 +1,62 @@
+"use client";
+
+import { useState } from "react";
+
 import NextIcon from "../icons/next-icon";
 import PreviousIcon from "../icons/previous-icon";
 import "./__styles.scss";
 
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 const Calendar = () => {
+  const date = new Date();
+  console.log("Hi date", date.getMonth());
+  // const currentMonth = date.getMonth();
+  const [currentMonth, setCurrentMonth] = useState(date.getMonth());
+  const [currentYear, setCurrentYear] = useState(date.getFullYear());
+  console.log(currentYear);
+
+  const handlePrevMonth = () => {
+    if (currentMonth > 0) {
+      setCurrentMonth(currentMonth - 1);
+    } else {
+      setCurrentYear(currentYear - 1);
+      setCurrentMonth(11);
+    }
+  };
+  const handleNextMonth = () => {
+    if (currentMonth < 11) {
+      setCurrentMonth(currentMonth + 1);
+    } else {
+      setCurrentYear(currentYear + 1);
+      setCurrentMonth(0);
+    }
+  };
+
   return (
     <div className="calendar-container">
       <div className="header">
-        <p className="month">July 2024</p>
+        <p className="month">
+          {monthNames[currentMonth]} {currentYear}
+        </p>
         <div className="buttons">
-          <button className="prev">
+          <button className="prev" onClick={handlePrevMonth}>
             <PreviousIcon className="icon" />
           </button>
-          <button className="next">
+          <button className="next" onClick={handleNextMonth}>
             <NextIcon className="icon" />
           </button>
         </div>
