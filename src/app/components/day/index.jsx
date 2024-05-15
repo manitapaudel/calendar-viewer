87f7f6760c131c3016c9ce0e2a4b1749f6e4e592
Manita Paudel <paudel04manita@gmail.com>
2024-05-15 22:34:50 +0545

    Move day scss to separate file, update component to take on event handler

--- src/app/components/day/index.jsx
+++ src/app/components/day/index.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
+
+import "./__styles.scss";
+
 const Day = ({ today, day }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
   return (
-    <span className={`day ${today && today === day ? "today" : ""}`}>
+    <span
+      className={`day ${today && today === day ? "today" : ""}`}
+      onClick={handleShowModal}
+    >
       <span className="text">{day}</span>
     </span>
   );
