fe59932d263a7fca56f0ab3c51e3e0405bac7ccd
Manita Paudel <paudel04manita@gmail.com>
2024-05-17 22:01:54 +0545

    Update date utils for handling exceptional cases

--- src/app/utils/getReadableDate.js
+++ src/app/utils/getReadableDate.js
@@ -2,9 +2,9 @@ import { monthNames } from "./constants";
 
 export const getReadableDate = (day, month, year) => {
   let suffix;
-  if (day % 10 === 1) suffix = "st";
-  else if (day % 10 === 2) suffix = "nd";
-  else if (day % 10 === 3) suffix = "rd";
+  if (day % 10 === 1 && day !== 11) suffix = "st";
+  else if (day % 10 === 2 && day !== 12) suffix = "nd";
+  else if (day % 10 === 3 && day !== 13) suffix = "rd";
   else suffix = "th";
 
   return `${day}${suffix} ${monthNames[month]}, ${year}`;
