935512085a7f38e1e16ae72af033041522c034b8
Manita Paudel <paudel04manita@gmail.com>
2024-05-17 16:41:40 +0545

    Add event handlers and states for the form, style input fields

--- src/app/modal/index.jsx
+++ src/app/modal/index.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 import CloseIcon from "../components/icons/close-icon";
 import "./__styles.scss";
 
 const Modal = ({ setShowModal }) => {
+  const [event, setEvent] = useState({
+    name: "",
+    description: "",
+    tag: "",
+  });
+
+  const handleChange = (e) => {
+    setEvent({
+      ...event,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div className="modal-overlay">
       <div className="modal-form-container">
@@ -14,10 +29,24 @@ const Modal = ({ setShowModal }) => {
             placeholder="Give your event a name"
             type="text"
             className="event-input"
+            name="name"
+            value={event.name}
+            onChange={handleChange}
           />
-          <input placeholder="" className="event-input" />
-          <input placeholder="" className="event-input" />
-          <input placeholder="" className="event-input" />
+          <input
+            placeholder="Describe the event in a few words"
+            className="event-input"
+            name="description"
+            value={event.description}
+            onChange={handleChange}
+          />
+          <input
+            placeholder="Highlight the importance of the event"
+            className="event-input"
+            name="tag"
+            value={event.tag}
+            onChange={handleChange}
+          />
           <button className="submit-btn">Add event</button>
         </form>
         <button className="close-modal" onClick={() => setShowModal(false)}>
