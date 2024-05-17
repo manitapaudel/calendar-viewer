985a6968b674a46cfb27b68b17084ee631f0b3b4
Manita Paudel <paudel04manita@gmail.com>
2024-05-17 15:54:02 +0545

    Add modal component, create form inside it

--- src/app/components/day/index.jsx
+++ src/app/components/day/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import "./__styles.scss";
+import Modal from "@/app/modal";
 
 const Day = ({ today, day }) => {
   const [showModal, setShowModal] = useState(false);
@@ -12,14 +13,17 @@ const Day = ({ today, day }) => {
   };
 
   return (
-    <span
-      className={`day ${today && today === day ? "today" : ""} ${
-        day !== "" ? "border" : ""
-      }`}
-      onClick={handleShowModal}
-    >
-      <span className="text">{day}</span>
-    </span>
+    <>
+      <span
+        className={`day ${today && today === day ? "today" : ""} ${
+          day !== "" ? "border" : ""
+        }`}
+        onClick={handleShowModal}
+      >
+        <span className="text">{day}</span>
+      </span>
+      {showModal ? <Modal setShowModal={setShowModal} /> : ""}
+    </>
   );
 };
 
---
+++ src/app/modal/index.jsx
@@ -0,0 +1,31 @@
+import CloseIcon from "../components/icons/close-icon";
+import "./__styles.scss";
+
+const Modal = ({ setShowModal }) => {
+  return (
+    <div className="modal-overlay">
+      <div className="modal-form-container">
+        <h2>Create an Event</h2>
+        <p className="greetings">
+          Add an event that you would like to remember.
+        </p>
+        <form className="event-form">
+          <input
+            placeholder="Give your event a name"
+            type="text"
+            className="event-input"
+          />
+          <input placeholder="" className="event-input" />
+          <input placeholder="" className="event-input" />
+          <input placeholder="" className="event-input" />
+          <button className="submit">Add event</button>
+        </form>
+        <button className="close-modal" onClick={() => setShowModal(false)}>
+          <CloseIcon className="close-icon" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
