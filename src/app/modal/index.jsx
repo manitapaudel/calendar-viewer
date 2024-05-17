8d33d6f971673e6516d6ce73a71f2548e06e920c
Manita Paudel <paudel04manita@gmail.com>
2024-05-17 22:22:15 +0545

    Add form validation styles and logic, update form to include error messages

--- src/app/modal/index.jsx
+++ src/app/modal/index.jsx
@@ -4,12 +4,15 @@ import CloseIcon from "../components/icons/close-icon";
 import { getReadableDate } from "../utils/getReadableDate";
 import "./__styles.scss";
 
+const initialState = {
+  name: "",
+  description: "",
+  tag: "",
+};
+
 const Modal = ({ setShowModal, day, currentMonth, currentYear }) => {
-  const [event, setEvent] = useState({
-    name: "",
-    description: "",
-    tag: "",
-  });
+  const [event, setEvent] = useState(initialState);
+  const [errors, setErrors] = useState(initialState);
   const readableDate = getReadableDate(day, currentMonth, currentYear);
 
   const handleChange = (e) => {
@@ -21,11 +24,19 @@ const Modal = ({ setShowModal, day, currentMonth, currentYear }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({
-      ...event,
-      createdDate: readableDate,
-    });
-    setShowModal(false);
+    if (event.name === "")
+      setErrors({ ...errors, name: "Please enter a name*" });
+    else if (event.description === "")
+      setErrors({ ...errors, description: "Please enter a description*" });
+    else if (event.tag === "")
+      setErrors({ ...errors, tag: "Please enter a tag*" });
+    else {
+      console.log({
+        ...event,
+        createdDate: readableDate,
+      });
+      setShowModal(false);
+    }
   };
 
   return (
@@ -36,31 +47,41 @@ const Modal = ({ setShowModal, day, currentMonth, currentYear }) => {
           Add an event that you would like to remember.
         </p>
         <form className="event-form">
-          <input
-            placeholder="Give your event a name*"
-            type="text"
-            className="event-input"
-            name="name"
-            value={event.name}
-            onChange={handleChange}
-            required
-          />
-          <input
-            placeholder="Describe the event in a few words*"
-            className="event-input"
-            name="description"
-            value={event.description}
-            onChange={handleChange}
-            required
-          />
-          <input
-            placeholder="Highlight the importance of the event*"
-            className="event-input"
-            name="tag"
-            value={event.tag}
-            onChange={handleChange}
-            required
-          />
+          <span className="event-input">
+            <input
+              placeholder="Give your event a name*"
+              type="text"
+              name="name"
+              value={event.name}
+              onChange={handleChange}
+              required
+            />
+            {errors.name && (
+              <span className="error-message">{errors.name}</span>
+            )}
+          </span>
+          <span className="event-input">
+            <input
+              placeholder="Describe the event in a few words*"
+              name="description"
+              value={event.description}
+              onChange={handleChange}
+              required
+            />
+            {errors.description && (
+              <span className="error-message">{errors.description}</span>
+            )}
+          </span>
+          <span className="event-input">
+            <input
+              placeholder="Highlight the importance of the event*"
+              name="tag"
+              value={event.tag}
+              onChange={handleChange}
+              required
+            />
+            {errors.tag && <span className="error-message">{errors.tag}</span>}
+          </span>
           <button className="submit-btn" onClick={handleSubmit}>
             Add event
           </button>
