521b8b3e67b7d5fef713a15598ef0d4b64bcf1a3
Manita Paudel <paudel04manita@gmail.com>
2024-05-19 22:13:01 +0545

    Check for the events in the localStorage for the specific days, add background colors to the days with events

--- src/app/components/day/index.jsx
+++ src/app/components/day/index.jsx
@@ -2,14 +2,18 @@ import { useState } from "react";
 
 import Modal from "@/app/modal";
 import "./__styles.scss";
-import { getRandomColor, getReadableDate } from "@/app/utils";
+import { getLocalStorage, getRandomColor, getReadableDate } from "@/app/utils";
 
 const Day = ({ today, day, currentMonth, currentYear }) => {
   const [showModal, setShowModal] = useState(false);
 
   const readableDate = getReadableDate(day, currentMonth, currentYear);
+  const events = getLocalStorage("events", []);
 
-  const hasEventOnThisDay = true;
+  const eventOfTheDay = events.find(
+    (event) => event.createdDate === readableDate
+  );
+
   const randomColor = getRandomColor();
 
   const handleShowModal = () => {
@@ -21,8 +25,13 @@ const Day = ({ today, day, currentMonth, currentYear }) => {
   return (
     <>
       <span
-        className={`day ${today && today === day ? "today" : ""}`}
-        style={{ backgroundColor: day && hasEventOnThisDay ? randomColor : "" }}
+        className={`day ${today && today === day ? "today" : ""} ${
+          day !== "" ? "border" : ""
+        }`}
+        style={{
+          backgroundColor:
+            day && eventOfTheDay !== undefined ? randomColor : "",
+        }}
         onClick={handleShowModal}
       >
         <span className="text">{day}</span>
