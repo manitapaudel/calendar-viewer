97af5fa3af8fdfa524574e5390aa483dca2efbbe
Manita Paudel <paudel04manita@gmail.com>
2024-05-23 11:34:15 +0545

    Remove unused code

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,8 @@
 import Calendar from "./components/calendar";
-import EventCard from "./components/event-card";
-
-const event = {
-  title: "Yoga Class",
-  description: "A yoga session with my buddies",
-  imgSrc: "/images/pattern-i.jpg",
-  createdAt: "4 hours ago",
-  tags: ["important"],
-  user: {
-    image: "",
-    name: "Sam Levinson",
-  },
-};
 
 export default function Home() {
   return (
     <div>
-      {/* <EventCard event={event} /> */}
       <Calendar />
     </div>
   );
