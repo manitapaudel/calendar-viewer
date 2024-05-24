1e8fd473d8420b529536a73a8b3affd79e01b031
Manita Paudel <paudel04manita@gmail.com>
2024-05-24 13:02:24 +0545

    Add outside click handler to the modal component

--- src/app/components/modal/index.jsx
+++ src/app/components/modal/index.jsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import { formInputs, initialState } from "@/app/utils/constants";
 import { getRandomColor } from "@/app/utils";
 import { CloseIcon } from "@/app/components/icons";
 import "./styles.scss";
+import useOutsideClickHandler from "@/app/hooks/useOutsideClickHandler";
 
 const Modal = ({
   setShowModal,
@@ -17,6 +18,9 @@ const Modal = ({
 }) => {
   const [event, setEvent] = useState(initialFormState);
   const [errors, setErrors] = useState(initialState);
+  const modalRef = useRef(null);
+
+  useOutsideClickHandler(modalRef, () => setShowModal(false));
 
   const handleChange = (e) => {
     setEvent({
@@ -55,7 +59,7 @@ const Modal = ({
 
   return (
     <div className="modal-overlay">
-      <div className="modal-form-container">
+      <div className="modal-form-container" ref={modalRef}>
         <h2>Create an Event for {readableDate}</h2>
         <p className="greetings">
           Add an event that you would like to remember.
