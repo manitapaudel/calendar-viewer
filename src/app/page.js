b092c07ab08416079789801d7c8fd5f88b628150
Manita Paudel <paudel04manita@gmail.com>
2024-05-25 11:47:14 +0545

    Create a simple onboarding page with form

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,39 @@
 
 import { useState } from "react";
 
+import "./globals.css";
+
 const Home = () => {
-  return <div>Hi there, welcome!</div>;
+  const [name, setName] = useState("");
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  return (
+    <div className="onboarding">
+      <h1>
+        Hey there! <span>Welcome.</span>
+      </h1>
+      <h2>
+        Let&apos;s start with your name, to get a more personalized feel to your
+        calendar
+      </h2>
+      <form>
+        <label>
+          {" "}
+          What would you like to be referred as?<sup>*</sup>
+        </label>
+        <input
+          type="text"
+          placeholder="Please enter a username"
+          value={name}
+          onChange={handleChange}
+          required
+        />
+        <button>Let&apos;s Go!</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
