96d1cbd9099ffb4789e9d83fcaa8b30f5859160c
Manita Paudel <paudel04manita@gmail.com>
2024-05-25 19:16:15 +0545

    Add condition to check whether the user is registered before viewing the calendar

--- src/app/my-calendar/page.js
+++ src/app/my-calendar/page.js
@@ -1,35 +1,41 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 import { ToastContext } from "@/app/utils/context";
 import Calendar from "@/app/components/calendar";
 import Toast from "@/app/components/toast";
+import { getLocalStorage } from "../utils";
 
 const MyCalendar = () => {
   const [showToast, setShowToast] = useState(false);
   const [message, setMessage] = useState("Event Created Successfully");
+  const userInfo = getLocalStorage("userInfo", null);
+  const router = useRouter();
 
   const handleShowToast = () => {
     setShowToast(true);
     setTimeout(() => setShowToast(false), 10000);
   };
 
-  return (
-    <div>
-      <Toast
-        message={message}
-        type="success"
-        extraClass={`${showToast ? "show" : ""}`}
-        setShowToast={setShowToast}
-      />
-      <ToastContext.Provider
-        value={{ showToast, setShowToast, handleShowToast, setMessage }}
-      >
-        <Calendar />
-      </ToastContext.Provider>
-    </div>
-  );
+  if (userInfo === null) router.push("/");
+  else
+    return (
+      <div>
+        <Toast
+          message={message}
+          type="success"
+          extraClass={`${showToast ? "show" : ""}`}
+          setShowToast={setShowToast}
+        />
+        <ToastContext.Provider
+          value={{ showToast, setShowToast, handleShowToast, setMessage }}
+        >
+          <Calendar />
+        </ToastContext.Provider>
+      </div>
+    );
 };
 
 export default MyCalendar;
