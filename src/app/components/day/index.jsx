ff2b773e0d6812cb6c2fe9563704233fd0af3b7d
Manita Paudel <paudel04manita@gmail.com>
2024-05-27 20:53:15 +0545

    Update the calendar for xs screen-sizes

--- src/app/components/day/index.jsx
+++ src/app/components/day/index.jsx
@@ -32,7 +32,7 @@ const Day = ({ today, day, currentMonth, currentYear }) => {
   useEffect(() => {
     if (day && hasEventInTheDay) {
       setEventStyle({
-        borderLeft: `10px solid ${eventOfTheDay.eventColor}`,
+        borderLeftColor: eventOfTheDay.eventColor,
       });
       setEventTextColor({
         color: eventOfTheDay.eventColor,
@@ -80,7 +80,7 @@ const Day = ({ today, day, currentMonth, currentYear }) => {
       <span
         className={`day ${today && today === day ? "today" : ""} ${
           day !== "" ? "border" : ""
-        }`}
+        } ${hasEventInTheDay ? "left-border" : ""}`}
         style={eventStyle}
         onClick={handleShowModal}
       >
